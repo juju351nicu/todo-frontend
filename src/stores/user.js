41f6646c99078c5ef7a5e6efe7c019af1d504daa
新谷健 <shintaniken@shintanikennoMacBook-puro.local>
2025-11-08 02:03:06 +0900

    fix user.js

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 import Const from "@/constants/const.js";
 import Fetcher from "@/utils/rest.js";
-import Util from "@/utils/util.js";
 export const useUserStore = defineStore("user", {
   state: () => ({
     memberId: 12,
@@ -16,15 +15,26 @@ export const useUserStore = defineStore("user", {
   }),
   getters: {},
   actions: {
+    /**
+     * アクセストークンチェックを設定する
+     * @param {String} token トークン文字列
+     */
     setAccessToken(token) {
       this.accessToken = token;
     },
     /**
      * ログインする
+     * @param {Object} payload リクエスト情報
+     * @returns ログイン結果
      */
     authLogin(payload) {
       return Fetcher.postRequest(Const.REST_PATH.AUTH_LOGIN, payload);
     },
+    /**
+     * トークンチェックを行う
+     * @param {String} _token トークン文字列
+     * @returns トークン判定結果
+     */
     validateToken(_token) {
       return Fetcher.getRequest(Const.REST_PATH.VALIDATE_TOKEN);
     },
