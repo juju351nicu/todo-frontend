dc825e885f1cd8876aa997e889f8052a4916adeb
新谷健 <shintaniken@shintanikennoMacBook-puro.local>
2026-08-08 13:30:12 +0900

    fix: Vue feature flag警告を解消する

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import vuetify from "vite-plugin-vuetify";
 import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
   server: {
     host: true,
     port: "8081",
